8b686af7bf9d108f11504a73e4bc9c29ea0c7429
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-15 12:27:29 +0100

    --- Auto-Git Commit ---

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class ChatScreen extends Component {
+    
+    static navigationOptions =({navigation})=>{
+        return{
+            title : navigation.getParam("name", null)
+        }
+    }
+
     constructor(props){
         super(props)
     }
