21e00d0c12005a6a4a29b3a5eb2fbb1a17a1f30b
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-15 12:25:13 +0100

    --- Auto-Git Commit ---

---
+++ screens/ChatScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class ChatScreen extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return (
+            <View>
+                <Text>
+                    Chat Screen
+                </Text>
+                
+            </View>
+        )
+    }
+}
