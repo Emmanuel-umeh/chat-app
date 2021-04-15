0237b368f557077256040362c4415b368773e1bc
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-15 12:48:45 +0100

    --- Auto-Git Commit ---

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { SafeAreaView, TextInput, View, Text, Button } from "react-native";
 
 import styles from "../constants/styles";
+import User from "../user";
 export default class ChatScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -11,18 +12,28 @@ export default class ChatScreen extends Component {
   };
 
   constructor(props) {
+
     super(props);
+
+    this.state = {
+        textMessage : ""
+    }
+    
   }
 
-state = {
-    textMessage : ""
-}
 
 sendMessage = async()=>{
     if(this.state.textMessage.length> 0){
         let msgID = firebase.database().ref("messages").
         child(User.phone).child(this.state.person.phone).push()
         .key
+
+        let updates = {}
+        let message ={
+            message : this.state.textMessage,
+            time : firebase.database.ServerValue.TIMESTAMP,
+            from : User.phone
+        }
     }
 } 
 
