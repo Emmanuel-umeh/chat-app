7146feecf97d9d414f504013ee8d28536fb2452a
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-15 11:57:26 +0100

    --- Auto-Git Commit ---

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,17 +4,21 @@ import { Button, Text, TouchableOpacity, View } from 'react-native'
 import User from '../user'
 import Styles from "../constants/styles"
 export default class HomeScreen extends Component {
+    constructor(props){
+        super(props)
+    }
 
     logout = async()=>{
         await AsyncStorage.clear()
         User.name =  null
         User.phone = null
+        this.props.navigation.navigate("Home")
 
     }
 
 
 
-    
+
     render() {
         return (
             <View style ={Styles.container}>
