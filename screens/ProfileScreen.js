09d5996768ad7783945b90b8513416ce2147ad5f
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-17 00:43:02 +0100

    --- Auto-Git Commit ---

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+export default class ProfileScreen extends Component {
+    render() {
+        return (
+            <View>
+
+                <Text>Profile</Text>
+                
+            </View>
+        )
+    }
+}
